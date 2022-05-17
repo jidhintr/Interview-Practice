7d3e8781d75baf0b205607a989f1eff6a059dc15
jidhin <jidhintr@gmail.com>
2022-05-17 19:34:46 +0200

    max2

---
+++ AlgorithmPattern/SlidingDoor/1.0_MaxSubArray/Program.cs
@@ -0,0 +1,29 @@
+﻿
+Console.WriteLine("Sliding door problem, refer ReadMe.md file for question");
+
+var arraySource = new[] { 2, 1, 5, 1, 3, 2 };
+var position = 3;
+var result = GetMaximumSubArray(arraySource, position);
+
+
+Console.WriteLine($"Result is {result}");
+Console.ReadLine();
+
+static int GetMaximumSubArray(int[] arraySource, int position)
+{
+    var sum = 0;
+    var tempSum = 0;
+    var piovtIndex = 0;
+    for (int i = 0; i < arraySource.Length; i++)
+    {
+        tempSum += arraySource[i];
+        if (i >= position - 1)
+        {
+            sum = Math.Max(tempSum, sum);
+            tempSum -= arraySource[piovtIndex];
+            piovtIndex++;
+        }
+    }
+    return sum;
+}
+
